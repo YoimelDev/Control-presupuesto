f65b4491108d0762cad489e7c702cb4594e4b754
Yoimel Rojas <yoimeldev@gmail.com>
2022-03-17 11:18:48 -0400

    add state nombre, cantidad and categoria

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import CerrarBtn from '../img/cerrar.svg'
 
 export const Modal = ({ setModal, animarModal, setAnimarModal }) => {
@@ -9,6 +10,10 @@ export const Modal = ({ setModal, animarModal, setAnimarModal }) => {
             setModal(false)
         }, 500);
     }
+
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState('')
+    const [categoria, setCategoria] = useState('')
 
     return (
         <div className="modal">
@@ -29,6 +34,8 @@ export const Modal = ({ setModal, animarModal, setAnimarModal }) => {
                         id="nombre"
                         type="text"
                         placeholder='Añade el nombre del gasto'
+                        value={nombre}
+                        onChange={e => setNombre(e.target.value)}
                     />
                 </div>
 
@@ -38,8 +45,34 @@ export const Modal = ({ setModal, animarModal, setAnimarModal }) => {
                         id="cantidad"
                         type="number"
                         placeholder='Añade la cantidad del gasto: ej. 300'
+                        value={cantidad}
+                        onChange={e => setCantidad(e.target.value)}
                     />
                 </div>
+
+                <div className='campo'>
+                    <label htmlFor="categoria">Categproa</label>
+
+                    <select
+                        id="categoria"
+                        value={categoria}
+                        onChange={e => setCategoria(e.target.value)}
+                    >
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="varios">Gastos Varios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                    </select>
+                </div>
+
+                <input
+                    type="submit"
+                    value="Añadir gasto"
+                />
             </form>
         </div>
     )
