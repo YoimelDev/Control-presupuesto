ba010f8f84ef7d2c1c0454106926ac1a8294b052
Yoimel Rojas <yoimeldev@gmail.com>
2022-03-18 10:30:57 -0400

    change option gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -86,7 +86,7 @@ export const Modal = ({ setModal, animarModal, setAnimarModal, guardarGasto }) =
                         <option value="ahorro">Ahorro</option>
                         <option value="comida">Comida</option>
                         <option value="casa">Casa</option>
-                        <option value="varios">Gastos Varios</option>
+                        <option value="gastos">Gastos Varios</option>
                         <option value="ocio">Ocio</option>
                         <option value="salud">Salud</option>
                         <option value="suscripciones">Suscripciones</option>
