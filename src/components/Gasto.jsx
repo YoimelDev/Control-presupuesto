5ffb9c478331ed958cb4ce2ee647bff529d9d6f9
Yoimel Rojas <yoimeldev@gmail.com>
2022-03-18 10:30:28 -0400

    add iconos and diccionarioIconos and images for the compenent gasto

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,5 +1,22 @@
 import React from 'react'
 import { formatearFecha } from '../helpers'
+import IconoAhorro from '../img/icono_ahorro.svg'
+import IconoCasa from '../img/icono_casa.svg'
+import IconoComida from '../img/icono_comida.svg'
+import IconoGastos from '../img/icono_gastos.svg'
+import IconoOcio from '../img/icono_ocio.svg'
+import IconoSuscripciones from '../img/icono_suscripciones.svg'
+import IconoSalud from '../img/icono_salud.svg'
+
+const diccionarioIconos = {
+    ahorro: IconoAhorro,
+    comida: IconoComida,
+    casa: IconoCasa,
+    gastos: IconoGastos,
+    ocio: IconoOcio,
+    salud: IconoSalud,
+    suscripciones: IconoSuscripciones
+}
 
 export const Gasto = ({ gasto }) => {
 
@@ -9,7 +26,10 @@ export const Gasto = ({ gasto }) => {
         <div className='gasto sombra'>
             <div className='contenido-gasto'>
 
-                {/* {Imagen} */}
+                <img
+                    src={diccionarioIconos[categoria]}
+                    alt={categoria}
+                />
 
                 <div className='descripcion-gasto'>
                     <p className='categoria'>{categoria}</p>
