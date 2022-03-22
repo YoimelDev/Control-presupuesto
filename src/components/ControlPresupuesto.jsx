9ab9be6f8deeeac876082a6900a93c8bd09ca8f4
Yoimel Rojas <yoimeldev@gmail.com>
2022-03-22 14:34:52 -0400

    add state disponible and gastado and modified useEffect

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export const ControlPresupuesto = ({ gastos, presupuesto }) => {
 
+    const [disponible, setDisponible] = useState(0)
+    const [gastado, setGastado] = useState(0)
+
     useEffect(() => {
-        first
-    }, [third])
+        const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
+
+        console.log(totalGastado);
+
+        setGastado(totalGastado)
+    }, [gastos])
 
 
     const formatearCantidad = (cantidad) => {
@@ -27,11 +34,11 @@ export const ControlPresupuesto = ({ gastos, presupuesto }) => {
                 </p>
 
                 <p>
-                    <span>Disponible: </span>{formatearCantidad(0)}
+                    <span>Disponible: </span>{formatearCantidad(disponible)}
                 </p>
 
                 <p>
-                    <span>Gastado: </span>{formatearCantidad(0)}
+                    <span>Gastado: </span>{formatearCantidad(gastado)}
                 </p>
             </div>
         </div>
